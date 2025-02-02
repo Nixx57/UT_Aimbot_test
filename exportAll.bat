d7013ba3daa678a9e978353d02cd7960bd1612d5
Nixx <rinamp57@hotmail.com>
2025-02-02 20:59:03 +0100

    Add Exporter + gitignore

---
+++ exportAll.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+set UT_DIR=C:\UnrealTournament
+set OUTPUT_DIR=C:\UnrealTournament\MyAimbot\export
+
+echo Starting source code extraction...
+
+if not exist "%OUTPUT_DIR%" (
+    echo Creating output directory: "%OUTPUT_DIR%"
+    mkdir "%OUTPUT_DIR%"
+)
+
+for %%F in ("%UT_DIR%\System\*.u") do (
+    echo Extracting source from: %%F
+	"%UT_DIR%\System\ucc" batchexport %%F class uc "%OUTPUT_DIR%"
+)
+
+echo Source code extraction complete.
+echo Files have been saved in "%OUTPUT_DIR%"
+
+pause
+endlocal
